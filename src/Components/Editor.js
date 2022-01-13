b4f10ab3178f5d7fbdcdd41f20cc791329dcdfb2
Josip Josic <71074064+Josipjosic@users.noreply.github.com>
2022-01-13 21:03:25 +0100

    Added validation / tried to post

--- src/Components/Editor.js
+++ src/Components/Editor.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import './Editor.scss';
+import { useRef, useState } from "react";
+import "./Editor.scss";
+import EditorCom from './EditorCom'
 
-function Editor () {
-    return (
-        <div className='EditorUi'>
-             <h2>Enter Info</h2>
-         <div className='EditorContent'>
-             <h3>ID</h3>
-             <input></input>
-             <h3>Name</h3>
-             <input></input>
-             <h3>Type</h3>
-             <input></input>
-             <h3>Year</h3>
-             <input></input>
-         </div>
-         <div className='EditorCom'>
-             <button>Confirm</button>
-             <button>Delete</button>
-         </div>
+const isEmpty = (value) => value.trim() === "";
+
+const Editor = (props) => {
+  const [formInputsValidity, setFormInputValidity] = useState({
+    id: true,
+    name: true,
+    type: true,
+  });
+
+  const idInputRef = useRef();
+  const nameInputRef = useRef();
+  const typeInputRef = useRef();
+
+  const confirmHandler = (event) => {
+    event.preventDefault();
+
+    const enteredId = idInputRef.current.value;
+    const enteredName = nameInputRef.current.value;
+    const enteredType = typeInputRef.current.value;
+
+    const enteredIdisValid = !isEmpty(enteredId);
+    const enteredNameisValid = !isEmpty(enteredName);
+    const enteredTypeisValid = !isEmpty(enteredType);
+
+    setFormInputValidity({
+      id: enteredIdisValid,
+      name: enteredNameisValid,
+      type: enteredTypeisValid,
+    });
+
+    const formIsValid =
+      enteredIdisValid && enteredNameisValid && enteredTypeisValid;
+
+    if (!formIsValid) {
+      return;
+    }
+
+  };
+  return (
+    <div className="EditorUi">
+      <h2>Enter Info</h2>
+      <form className="EditorContent" onSubmit={confirmHandler}>
+        <label htmlFor="id">ID</label>
+        <input type="text" id="id" ref={idInputRef}></input>
+        {!formInputsValidity.id && <p>please enter a ID</p>}
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" ref={nameInputRef}></input>
+        {!formInputsValidity.name && <p>please enter a name</p>}
+        <label htmlFor="type">Type</label>
+        <input type="text" id="type" ref={typeInputRef}></input>
+        {!formInputsValidity.type && <p>please enter a type</p>}
+        <EditorCom onSubmitHandler={EditorCom}></EditorCom>
+      </form>
     </div>
-    )
-}
-
+  );
+};
 export default Editor;
---
+++ src/Components/EditorCom.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './EditorCom.scss'
+
+const EditorCom = () => {
+
+  return (
+    <div className="EditorCom">
+      <button>Confirm</button>
+      <button type="button">Delete</button>
+    </div>
+  );
+};
+
+export default EditorCom;
